2dae8a7db1b29e7bc8f065e20fad767a3f311bd7
flitscha <felixcampidell@gmail.com>
2025-04-24 13:21:12 +0200

    fixed the goal-querying error, by using unique request_id

--- add_metadata.py
+++ add_metadata.py
@@ -5,8 +5,8 @@ async def add_goals_to_proof_steps(lean_session, proof_steps):
     """
     for step in proof_steps:
         for line in step.lean_code:
-            goal_before = await lean_session.get_goal_at_position(line.line_number, 1)
-            goal_after = await lean_session.get_goal_at_position(line.line_number, len(line.lean_line))
+            goal_before = await lean_session.get_goal_at_position(line.line_number, 1, request_id=line.line_number*2)
+            goal_after = await lean_session.get_goal_at_position(line.line_number, len(line.lean_line), request_id=line.line_number*2+1)
 
             cleaned_goal_before = goal_before.replace("```lean", "").replace("```", "").strip()
             cleaned_goal_after = goal_after.replace("```lean", "").replace("```", "").strip()
