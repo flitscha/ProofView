677c37d3614ddaa6dfc397a3103bbf7302a33e22
flitscha <felixcampidell@gmail.com>
2025-04-24 16:34:50 +0200

    bugfix in the syntax highlighter

--- syntax_highlighter.py
+++ syntax_highlighter.py
@@ -8,12 +8,22 @@ def highlight_goal(goal_str: str) -> str:
     """
 
     # make the case-line green
-    html = re.sub(r"^(case\s+[^\n]+)", r'<span class="goal-case">\1</span>', goal_str, flags=re.MULTILINE)
+    html = re.sub(
+        r"^(case\s+[^\n]+)", 
+        r'<span class="goal-case">\1</span>', 
+        goal_str, 
+        flags=re.MULTILINE
+    )
+
+    # everything before the first colon on a line becomes orange (goal-id)
+    html = re.sub(
+        r"^(?!\s*⊢)(.*?)(\s*:\s*)(.*)$",  # not the goal line (⊢)
+        r'<span class="goal-id">\1</span>\2\3',
+        html,
+        flags=re.MULTILINE
+    )
 
     # make turnstile ⊢ blue
     html = html.replace("⊢", '<span class="goal-turnstile">⊢</span>')
-
-    # everything before the first colon on a line becomes yellow (goal-id)
-    html = re.sub(r"^(.*?)(\s*:\s*)(.*)$", r'<span class="goal-id">\1</span>\2\3', html, flags=re.MULTILINE)
 
     return f'<pre class="goal-pre">{html}</pre>'
